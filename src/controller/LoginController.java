48f28f5e44b6f6af7e32678e08070e2ac5f53045
oFelipeee <jufpaulino@gmail.com>
2024-05-10 16:15:12 -0300

    .

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,9 +1,16 @@
 package controller;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class LoginController {
 	
@@ -13,11 +20,34 @@ public class LoginController {
 	@FXML
 	private PasswordField senha;
 	
+	public Stage primaryStage;
+	
 	public void entrar() {
 		System.out.println("Tentaiva de entrar!");
 		
 		if (usuario.getText().equals("admin") && senha.getText().equals("admin")) {
-			System.out.println("Realizando Login");
+		
+			try {
+				
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/dashboard.fxml"));
+				
+			StackPane root = new StackPane();
+			
+			loader.setRoot(root);
+			loader.load();
+				
+				Scene scene = new Scene(root, 600, 400);
+				
+				Stage currentStage = (Stage) usuario.getScene().getWindow();
+				currentStage.close();
+				
+				currentStage.setScene(scene);
+				currentStage.setTitle("dashboard");
+				currentStage.show();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
 		}else {
 			mensagemDeErro();
 		}
